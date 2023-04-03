2495d62043b2ecfc8543bc36eb8d156633c2be6e
김명식 <rlaaudtlr233@naver.com>
2023-04-03 19:57:09 +0900

    layout.tsx 공부

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import React from 'react'
+import Link from 'next/link';
 
 type Props = {
   // 동적라우팅을 위한 next.js props
@@ -12,7 +13,9 @@ type Props = {
 export default function SlugPage({ params }: Props) {
   if (params.slug === 'nothing') notFound();
   return (
-    <h1>{ params.slug }제품 페이지</h1>
+    <>
+      <h1>{params.slug}제품 페이지</h1>
+    </>
   )
 }
 // 동적라우트 방식에서 미리 만들어놀 페이지 경로를 만들어주는 next.js 내장 함수
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
+
+const products = ['shirt', 'pants', 'skirt', 'shoes']
 
 export default function ProductsPage() {
   return (
-    <h1>제품 소개 페이지!</h1>
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map((product, index) => {
+          return <li key={index}><Link href={`/products/${product}`}>{ product }</Link></li>
+        })}
+      </ul>
+    </>
   )
 }
