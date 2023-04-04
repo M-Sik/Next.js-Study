c06f162151178e0a872c0eb780ff636edf58f0c3
김명식 <rlaaudtlr233@naver.com>
2023-04-04 23:28:39 +0900

    next redirect 주석 추가가

--- next.config.js
+++ next.config.js
@@ -12,6 +12,8 @@ const nextConfig = {
       }
     ]
   },
+  // source에 적어둔 경로로 접근시 destination에 적어둔 경로로 리다이렉트 시킴
+  // permaent옵션은 한번 리다이렉트가 일어난 주소면 브라우저에 캐싱해서 다시 확인하지말고 리다이렉트 시키라는 옵션
   async redirects() {
     return [
       {
