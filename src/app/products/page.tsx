6dace06201357edce176fd00cacda4d5e5ee1bd9
김명식 <rlaaudtlr233@naver.com>
2023-04-04 16:59:25 +0900

    fetch api를 통한 ssg isg ssr 방법 공부

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,11 +3,19 @@ import Link from 'next/link'
 import React from 'react'
 
 // ISR 방식 3초마다 새로운 데이터로 렌더링
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   // 서버 파일(데이터 베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+  // revalidate 혹은 chche를 통한 다른 설정을 하지 않는다면 ssg가 됨
+  // revalidate: 0, 혹은 cache: 'no-store'를 사용하게 된다면 ssr과 같아짐
   const products = await getProducts();
+  const res = await fetch('https://meowfacts.herokuapp.com', {
+    // cache: 'no-store',
+    // next: { revalidate: 3 },
+  })
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -16,6 +24,9 @@ export default async function ProductsPage() {
           return <li key={index}><Link href={`/products/${product.id}`}>{ product.name }</Link></li>
         })}
       </ul>
+      <article>
+        {factText}
+      </article>
     </>
   )
 }
