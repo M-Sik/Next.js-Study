79a46f8662a6d9c36e8839132520ab649a43a624
김명식 <rlaaudtlr233@naver.com>
2023-04-04 16:32:04 +0900

    ISR 방식 공부 - 상단에 exprot const revalidate 라는 키워드로 사용 가능 revalidate = 3; 일 경우 3초마다 새로운 데이터로 렌더링 한다는 것임

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import { notFound } from 'next/navigation';
 import React from 'react'
 import Link from 'next/link';
+import { getProduct, getProducts } from '@/apis/products';
+
+// ISR 방식 3초마다 새로운 데이터로 렌더링
+export const revalidate = 3;
 
 type Props = {
   // 동적라우팅을 위한 next.js props
@@ -18,18 +22,25 @@ export function generateMetadata({ params }: Props) {
   }
 }
 
-export default function SlugPage({ params }: Props) {
-  if (params.slug === 'nothing') notFound();
+export default async function ProductPage({ params: {slug} }: Props) {
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+  const product = await getProduct(slug);
+  // 만약 찾는 제품이 없으면 notFound
+  if (!product) {
+    notFound()
+  }
+
   return (
     <>
-      <h1>{params.slug}제품 페이지</h1>
+      <h1>{product.name}제품 페이지</h1>
     </>
   )
 }
 // 동적라우트 방식에서 미리 만들어놀 페이지 경로를 만들어주는 next.js 내장 함수
-export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있도록 해줄 것임 (SSG)
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }))
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,19 @@
+import { getProducts } from '@/apis/products'
 import Link from 'next/link'
 import React from 'react'
 
-const products = ['shirt', 'pants', 'skirt', 'shoes']
+// ISR 방식 3초마다 새로운 데이터로 렌더링
+export const revalidate = 3;
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  // 서버 파일(데이터 베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
         {products.map((product, index) => {
-          return <li key={index}><Link href={`/products/${product}`}>{ product }</Link></li>
+          return <li key={index}><Link href={`/products/${product.id}`}>{ product.name }</Link></li>
         })}
       </ul>
     </>
